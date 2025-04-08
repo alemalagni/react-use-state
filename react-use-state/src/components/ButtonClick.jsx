d21161977925d91218d6124621037a2f2bf313ee
alemalagni <alemalagni@gmail.com>
2025-04-08 22:40:14 +0200

    refactor(ButtonClick): removed

--- react-use-state/src/components/ButtonClick.jsx
+++
@@ -1,7 +0,0 @@
-export default function ButtonClick({ id, title }) {
-    return (
-        <div>
-            <button id={id} type="button" className="btn btn-warning">{title}</button>
-        </div>
-    );
-}
