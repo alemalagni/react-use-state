3fde7aa9d47e23b4e16ff32632521e0b81e0c631
alemalagni <alemalagni@gmail.com>
2025-04-08 22:51:28 +0200

    feat(Description): add script

--- react-use-state/src/components/Description.jsx
+++ react-use-state/src/components/Description.jsx
@@ -1,9 +1,13 @@
 import languages from "../languages";
 
 export default function Description({ selected }) {
+    const title = languages[selected].title;
+    const description = languages[selected].description;
+
     return (
         <div className="border mt-4 ms-3 me-3 rounded-2 p-1">
-
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
     );
 }
