412b23c4588c3642c2b10eae6d2d1e855eb2bb42
alemalagni <alemalagni@gmail.com>
2025-04-08 16:32:16 +0200

    feat(Main):

--- react-use-state/src/components/Main.jsx
+++ react-use-state/src/components/Main.jsx
@@ -4,7 +4,7 @@ import languages from "../languages.js";
 export default function Main() {
     return (
         <main>
-            <div className="d-flex gap-1">
+            <div className="d-flex gap-2 justify-content-center pt-3">
                 {languages.map((item) => (
                     <MainButton
                         title={item.title}
