3c51c32d22936681955eb24af49747f3f886dc14
alemalagni <alemalagni@gmail.com>
2025-04-08 23:16:49 +0200

    fix(MainButton):

--- react-use-state/src/components/MainButton.jsx
+++ react-use-state/src/components/MainButton.jsx
@@ -4,8 +4,10 @@ export default function MainButton({ selected, onSelect }) {
     return (
         <div className="d-flex gap-2 justify-content-center pt-3">
             {languages.map((item) => (
-                <button type="button"
+                <button
+                    type="button"
                     key={item.id}
+                    id={item.id}
                     onClick={() => onSelect(item.id)}
                     className={selected === item.id ? "btn btn-warning" : "btn btn-primary"}
                 >
