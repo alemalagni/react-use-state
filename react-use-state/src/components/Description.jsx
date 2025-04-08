ae1eab7c169623f598ed994cd2e134c12d0b4a11
alemalagni <alemalagni@gmail.com>
2025-04-08 22:40:35 +0200

    feat(Description): add import

--- react-use-state/src/components/Description.jsx
+++ react-use-state/src/components/Description.jsx
@@ -1,7 +1,9 @@
-export default function Description({ text }) {
+import languages from "../languages";
+
+export default function Description({ selected }) {
     return (
         <div className="border mt-4 ms-3 me-3 rounded-2 p-1">
-            {text}
+
         </div>
     );
 }
